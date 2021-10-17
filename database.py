9c21f32891c4fa6f2a30f165ae51eefb3a9cbdb4
Cong Thanh Vu <zarelasez@gmail.com>
2021-10-17 12:47:56 +0700

    Update database.py

--- database.py
+++ database.py
@@ -5,17 +5,21 @@ def getDbUri():
     db_user = 'yell-backend'
     db_pass = '8SA46Wil8iEJt|9]z'
     db_name = 'yell'
-    db_host = '34.69.77.182:3306'
+    db_host = '10.80.144.4:3306'
 
-    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-    cloud_sql_connection_name = 'learning-327601:us-central1:yell-data'
+    host_args = db_host.split(":")
+    if len(host_args) == 1:
+        db_hostname = db_host
+        db_port = os.environ["DB_PORT"]
+    elif len(host_args) == 2:
+        db_hostname, db_port = host_args[0], int(host_args[1])
 
-    URI = '{}://{}:{}@{}?unix_sock={}/{}/.s.PGSQL.5432'.format(
+    URI = '{}://{}:{}@{}:{}/{}?charset={}'.format(
             "postgresql+pg8000",
             db_user,
             db_pass,
+            db_hostname,
+            db_port,
             db_name,
-            db_socket_dir,
-            cloud_sql_connection_name,
-            )
+            'utf8')
     return URI
