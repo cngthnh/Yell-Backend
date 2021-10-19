0ec27f51cd9cd0cdccd4c61ed9740f1346ca021d
Cong Thanh Vu <zarelasez@gmail.com>
2021-10-19 13:44:13 +0700

    email style change

--- utils/email.py
+++ utils/email.py
@@ -16,8 +16,9 @@ def sendEmailAsync(msg):
     sender.start()
 
 def sendVerificationEmail(recipient, token, recipient_name):
-    msg = Message(subject = 'Yell Account Verification', sender = ('Yell', os.environ.get('MAIL_USERNAME')), recipients = [recipient])
+    sender_mail = os.environ.get('MAIL_USERNAME')
+    msg = Message(subject = 'Yell Account Verification', sender = ('Yell', sender_mail), recipients = [recipient])
     link = os.environ.get('YELL_MAIN_URL', '') + EMAIL_VRF_ENDPOINT + token
-    msg.html = render_template(EMAIL_VRF_TEMPLATE_HTML, link = link, name = recipient_name)
-    msg.body = render_template(EMAIL_VRF_TEMPLATE_TXT, link = link, name = recipient_name)
+    msg.html = render_template(EMAIL_VRF_TEMPLATE_HTML, link = link, name = recipient_name, mail=sender_mail)
+    msg.body = render_template(EMAIL_VRF_TEMPLATE_TXT, link = link, name = recipient_name, mail=sender_mail)
     sendEmailAsync(msg)
