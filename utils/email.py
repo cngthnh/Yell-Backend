f62d2437ebcf29b102ab629e9949f020ccf5e2dc
Cong Thanh Vu <zarelasez@gmail.com>
2021-10-19 13:49:39 +0700

    edit template

--- utils/email.py
+++ utils/email.py
@@ -20,5 +20,5 @@ def sendVerificationEmail(recipient, token, recipient_name):
     msg = Message(subject = 'Yell Account Verification', sender = ('Yell', sender_mail), recipients = [recipient])
     link = os.environ.get('YELL_MAIN_URL', '') + EMAIL_VRF_ENDPOINT + token
     msg.html = render_template(EMAIL_VRF_TEMPLATE_HTML, link = link, name = recipient_name, mail=sender_mail)
-    msg.body = render_template(EMAIL_VRF_TEMPLATE_TXT, link = link, name = recipient_name, mail=sender_mail)
+    msg.body = render_template(EMAIL_VRF_TEMPLATE_TXT, link = link, name = recipient_name)
     sendEmailAsync(msg)
