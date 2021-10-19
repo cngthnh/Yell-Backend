7dfdec33b4f0a123d669dd26df5aa5bd2381ecd3
Cong Thanh Vu <zarelasez@gmail.com>
2021-10-19 12:27:23 +0700

    Update yell_backend.py

--- yell_backend.py
+++ yell_backend.py
@@ -1,5 +1,5 @@
-from utils.loader import *
-    
+from routes import *
+
 if __name__ == '__main__':
     db.create_all()
     app.run(ssl_context='adhoc', host='0.0.0.0', port=os.environ.get('PORT', 80))
