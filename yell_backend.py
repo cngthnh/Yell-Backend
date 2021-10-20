690efd9d2cdbf31f0dfc764c07f24b935a656fe3
Cong Thanh Vu <zarelasez@gmail.com>
2021-10-20 16:42:24 +0700

    try

--- yell_backend.py
+++ yell_backend.py
@@ -2,9 +2,10 @@ from sqlalchemy.exc import InvalidRequestError
 from core.routes import *
 
 if __name__ == '__main__':
-    db.create_all()
     try:
-        db.session.commit()
+        with app.app_context():
+            db.create_all()
+            db.session.commit()
     except InvalidRequestError:
         db.session.rollback()
         raise Exception('cant create tables')
