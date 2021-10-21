b872760ec86895b7cf29a819821e9ff3e10c015a
Cong Thanh Vu <zarelasez@gmail.com>
2021-10-21 21:24:14 +0700

    Update loader.py

--- core/loader.py
+++ core/loader.py
@@ -57,7 +57,7 @@ app = Flask(__name__)
 loadConfigs()
 
 # init SQL database and email connection
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql+pg8000')
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
