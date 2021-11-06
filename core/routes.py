ed5390ad19d26b9ce828543ca49dd1b20cbb7769
Cong Thanh Vu <zarelasez@gmail.com>
2021-11-06 12:17:42 +0700

    Alpha v1.3.2a
    - Added Refresh Token rotation
    - Added Session management
    - Added RT reuse detection

--- core/routes.py
+++ core/routes.py
@@ -29,17 +29,68 @@ def tokenRequired(func):
             if (tokenDict[API_TOKEN_TYPE]!=ACCESS_TOKEN_TYPE):
                 return jsonify(message=ACCESS_TOKEN_REQUIRED_MESSAGE), 403
 
+            session = db.session.query(Session).filter_by(id=tokenDict[SESSION_ID_KEY]).first()
+
+            if session is None:
+                return jsonify(message=INVALID_SESSION_MESSAGE), 403
+
+            if (int(session.updated_at.timestamp())!=tokenDict[ISSUED_AT_KEY]):
+                try:
+                    db.session.delete(session)
+                    db.session.commit()
+                except Exception:
+                    db.session.rollback()
+                return jsonify(message=INVALID_SESSION_MESSAGE), 403
+
         except jwt.ExpiredSignatureError:
             return jsonify(message=EXPIRED_TOKEN_MESSAGE), 401
         except Exception:
             return jsonify(message=INVALID_TOKEN_MESSAGE), 403
         
-        return func(*args, **kwargs, uid=tokenDict[API_UID])
+        return func(*args, **kwargs, uid=session.user_id)
     return tokenCheck
 
 @app.route('/')
 def homepage():
     return 'Yell by Yellion'
+
+@app.route(AUTH_ENDPOINT, methods=['DELETE'])
+def logout():
+    try:
+        token = request.headers['Authorization']
+    except Exception:
+        return jsonify(message=INVALID_TOKEN_MESSAGE), 403
+
+    schema, token = token.split(maxsplit=1)
+    if schema!='Bearer':
+        return jsonify(message=INVALID_TOKEN_MESSAGE), 403
+
+    # parse token => dict of info
+    try:
+        tokenDict = decode(token)
+        if tokenDict[ISSUER_KEY] != YELL_ISSUER:
+            return jsonify(message=INVALID_TOKEN_MESSAGE), 403
+        if (tokenDict[API_TOKEN_TYPE]!=ACCESS_TOKEN_TYPE):
+            return jsonify(message=ACCESS_TOKEN_REQUIRED_MESSAGE), 403
+
+        session = db.session.query(Session).filter_by(id=tokenDict[SESSION_ID_KEY]).first()
+
+        if session is None:
+            return jsonify(message=INVALID_SESSION_MESSAGE), 403
+            
+        try:
+            db.session.delete(session)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            return jsonify(message=INVALID_SESSION_MESSAGE), 403
+
+    except jwt.ExpiredSignatureError:
+        return jsonify(message=EXPIRED_TOKEN_MESSAGE), 401
+    except Exception:
+        return jsonify(message=INVALID_TOKEN_MESSAGE), 403
+    
+    return jsonify(message=SUCCEED_MESSAGE), 200
 
 @app.route(AUTH_ENDPOINT, methods=['POST'])
 def getToken():
@@ -57,14 +108,26 @@ def getToken():
 
     try:
         if checkAccount(_uid, _hash):
-            _accessTokenDict = {API_UID: _uid, API_TOKEN_TYPE: ACCESS_TOKEN_TYPE}
-            _refreshTokenDict = {API_UID: _uid, API_TOKEN_TYPE: REFRESH_TOKEN_TYPE}
+            session = Session(_uid)
+
+            try:
+                db.session.add(session)
+                db.session.commit()
+            except Exception:
+                db.session.rollback()
+                return jsonify(message=INVALID_SESSION_MESSAGE), 403
+
+            _accessTokenDict = {API_TOKEN_TYPE: ACCESS_TOKEN_TYPE, SESSION_ID_KEY: str(session.id)}
+            _refreshTokenDict = {API_TOKEN_TYPE: REFRESH_TOKEN_TYPE, SESSION_ID_KEY: str(session.id)}
+
             return jsonify(
-                        access_token=encode(_accessTokenDict, ACCESS_TOKEN_EXP_TIME),
-                        refresh_token=encode(_refreshTokenDict)
+                        access_token=encode(_accessTokenDict, ACCESS_TOKEN_EXP_TIME, iat=session.updated_at),
+                        refresh_token=encode(_refreshTokenDict, iat=session.updated_at)
                         ), 200
         return jsonify(message=INACTIVATED_ACCOUNT_MESSAGE), 401
-    except Exception:
+    except Exception as e:
+        print(e)
+        sys.stdout.flush()
         return jsonify(message=INVALID_CREDENTIALS_MESSAGE), 401
 
 @app.route(AUTH_ENDPOINT, methods=['GET'])
@@ -86,10 +149,42 @@ def refreshToken():
         if (tokenDict[API_TOKEN_TYPE]!=REFRESH_TOKEN_TYPE):
             return jsonify(message=REFRESH_TOKEN_REQUIRED_MESSAGE), 403
         
-        _accessTokenDict = {API_UID: tokenDict[API_UID], API_TOKEN_TYPE: ACCESS_TOKEN_TYPE}
-        return jsonify(access_token=encode(_accessTokenDict, ACCESS_TOKEN_EXP_TIME))
+        session = db.session.query(Session).filter_by(id=tokenDict[SESSION_ID_KEY]).first()
+        if (session is None):
+            return jsonify(message=INVALID_SESSION_MESSAGE), 403
+        
+        if (int(session.updated_at.timestamp())!=tokenDict[ISSUED_AT_KEY]):
+            try:
+                db.session.delete(session)
+                db.session.commit()
+            except Exception:
+                db.session.rollback()
+            return jsonify(message=INVALID_SESSION_MESSAGE), 403
+
+        iat = datetime.utcnow()
+        session.updated_at = iat
+
+        try:
+            db.session.add(session)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            return jsonify(message=INVALID_SESSION_MESSAGE), 403
+
+        _refreshTokenDict = {API_TOKEN_TYPE: REFRESH_TOKEN_TYPE, SESSION_ID_KEY: str(session.id)}
+        _accessTokenDict = {API_TOKEN_TYPE: ACCESS_TOKEN_TYPE, SESSION_ID_KEY: str(session.id)}
+
+        return jsonify(access_token=encode(_accessTokenDict, ACCESS_TOKEN_EXP_TIME, iat=iat), 
+                        refresh_token=encode(_refreshTokenDict, iat=iat)), 200
 
     except jwt.ExpiredSignatureError:
+        session = db.session.query(Session).filter_by(id=tokenDict[SESSION_ID_KEY]).first()
+        if (session is not None):
+            try:
+                db.session.delete(session)
+                db.session.commit()
+            except Exception:
+                db.session.rollback()
         return jsonify(message=EXPIRED_TOKEN_MESSAGE), 401
     except Exception:
         return jsonify(message=INVALID_TOKEN_MESSAGE), 403
