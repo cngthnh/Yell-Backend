22fbb3b8a77ff77594bc2029a5573eecc2268597
Cong Thanh Vu <zarelasez@gmail.com>
2021-11-26 20:35:08 +0700

    Update auth_bp.py

--- core/routes/auth_bp.py
+++ core/routes/auth_bp.py
@@ -3,4 +3,6 @@ from ..controllers.auth_controller import *
 from flask import Blueprint
 
 auth_bp = Blueprint('auth_bp', __name__)
-auth_bp.route('/', methods=['GET'])(authorized)
+auth_bp.route('/', methods=['GET'])(refreshToken)
+auth_bp.route('/', methods=['POST'])(getToken)
+auth_bp.route('/', methods=['DELETE'])(logout)
