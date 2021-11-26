34119646e3cfa57087c15cbc9ba0cda6ab20933f
Cong Thanh Vu <zarelasez@gmail.com>
2021-11-26 18:42:33 +0700

    try

--- core/routes/auth_bp.py
+++ core/routes/auth_bp.py
@@ -2,5 +2,5 @@ from ..controllers.auth_controller import *
 
 from flask import Blueprint
 
-auth_bp = Blueprint('user_bp', __name__)
+auth_bp = Blueprint('auth_bp', __name__)
 auth_bp.route('/', methods=['GET'])(authorized)
--- yell_backend.py
+++ yell_backend.py
@@ -3,4 +3,5 @@ from core.routes.auth_bp import auth_bp
 
 if __name__ == '__main__':
     app.register_blueprint(auth_bp, url_prefix=AUTH_ENDPOINT)
+    print(app.blueprints.keys())
     app.run(ssl_context='adhoc', host='0.0.0.0', port=os.environ.get('PORT', 443))
