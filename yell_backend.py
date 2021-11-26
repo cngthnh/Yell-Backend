2bfaf716b8f40c36ffbc7f70778fdd9626713c9b
Cong Thanh Vu <zarelasez@gmail.com>
2021-11-26 18:49:06 +0700

    Update yell_backend.py

--- yell_backend.py
+++ yell_backend.py
@@ -5,6 +5,6 @@ from core.routes.auth_bp import auth_bp
 if __name__ == '__main__':
     app.register_blueprint(auth_bp, url_prefix=AUTH_ENDPOINT)
     print("BLUEPRINTS HERE")
-    print(app.blueprints.keys())
+    raise Exception(app.blueprints.keys())
     sys.stdout.flush()
     app.run(ssl_context='adhoc', host='0.0.0.0', port=os.environ.get('PORT', 443))
