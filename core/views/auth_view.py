05c5098cee56c53496d3af79f8f3a1faf5c1143a
Cong Thanh Vu <zarelasez@gmail.com>
2021-12-14 16:21:35 +0700

    Update auth_view.py

--- core/views/auth_view.py
+++ core/views/auth_view.py
@@ -1,3 +1,4 @@
+from core.views.message_view import getMessage
 from ..models.models import Session
 from .json_view import makeJson
 from ..utils.cipher import *
@@ -8,5 +9,5 @@ def genTokenPair(session: Session) -> str:
     _accessTokenDict = {API_TOKEN_TYPE: ACCESS_TOKEN_TYPE, SESSION_ID_KEY: str(session.id)}
     _refreshTokenDict = {API_TOKEN_TYPE: REFRESH_TOKEN_TYPE, SESSION_ID_KEY: str(session.id)}
     
-    return makeJson(access_token=encode(_accessTokenDict, ACCESS_TOKEN_EXP_TIME, iat=session.updated_at),
+    return getMessage(access_token=encode(_accessTokenDict, ACCESS_TOKEN_EXP_TIME, iat=session.updated_at),
                         refresh_token=encode(_refreshTokenDict, iat=session.updated_at))
