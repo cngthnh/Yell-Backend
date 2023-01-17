0b9b46bad83328ec9471c6a867329f45ad857932
Cong Thanh Vu <congthanh3181@gmail.com>
2023-01-17 16:52:31 +0700

    Update loader.py

--- core/loader.py
+++ core/loader.py
@@ -65,7 +65,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['DEBUG'] = True
 
 # init DB
